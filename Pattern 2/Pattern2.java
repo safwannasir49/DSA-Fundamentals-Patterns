4ddd747577162f8d97f6d4cbf53ab03388380afe
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-02-06 12:57:07 +0530

    Create Pattern2.java

---
+++ Pattern 2/Pattern2.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void nForest(int n) {
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        }
+    }
